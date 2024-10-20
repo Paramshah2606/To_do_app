f945b15024d5d0e1f0bd0436397636f835766e3a
Paramshah2606 <paramshah2606@gmail.com>
2024-10-20 23:39:59 +0530

    CSS Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-indigo-900 text-white py-5'>
+    <nav className='flex justify-around bg-indigo-900 text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-3xl mx-8'>iTask</span>
+            <span className='font-bold text-xl mx-8'>iTask</span>
         </div>
-      <ul className="flex gap-20 mx-11">
-        <li className='cursor-pointer hover:font-bold transition-all text-3xl'>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all text-3xl'>Your Tasks</li>
+      <ul className="flex gap-8 mx-9">
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
       </ul>
     </nav>
   )
